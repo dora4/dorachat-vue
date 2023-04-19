a8e03f3577c7ac7338f2439b92ead60294073dce
dora4 <71242257+dora4@users.noreply.github.com>
2023-04-20 01:50:49 +0800

    DoraChat官网最初代码

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      有朋友，不孤单，欢迎使用DoraChat!
+      <a href="https://github.com/dora4/" target="_blank" rel="noopener">GitHub</a> +
+      <a href="https://juejin.cn/user/2656895088205453" target="_blank" rel="noopener">稀土掘金技术社区</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>
+      <a href="https://dora4.oss-cn-hangzhou.aliyuncs.com/app-alpha-debug.apk">点击下载DoraChat内测版本APK</a>
+    </h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
