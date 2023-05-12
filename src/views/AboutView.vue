41abda5fe17cef766dacf7388c9e97b0240f9dff
dora4 <71242257+dora4@users.noreply.github.com>
2023-05-12 20:44:01 +0800

    更换logo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="about">
     <h1>
+        <p>
+         隐私保险箱5月12日的激活码：X/j5QoCJj4p7Vruh/6On98If3p5C5KFNceZxlhijAEoB/1a1d+n95+UVBXXvDlZnwvPycndwh71wbc4GqFD6CutvKJ+aChznD3OeX01jU4HXnYPIrX0aqtdPamYIA9CPXOOoJstJxUSF/vhMJJXX2MQTXYbpkRkwnSeXymZgOPI=
+        </p>
       <p>
-      <a href="https://www.pgyer.com/privacybox">隐私保险箱，DoraChat出品</a>
+      <a href="https://www.pgyer.com/privacybox">
+        隐私保险箱下载地址
+        </a>
       </p>
+
       <p>
       <a href="https://www.pgyer.com/dorachat">点击下载DoraChat内测版本APK</a>
       </p>
