d3cfda463bc1066accd31f5bf4a08882086316b0
dora4 <71242257+dora4@users.noreply.github.com>
2023-05-27 01:34:42 +0800

    调整激活码界面

--- src/views/ActivateView.vue
+++ src/views/ActivateView.vue
@@ -1,33 +1,322 @@
 <template>
   <div class="about">
+
+    <br/>
+
     <h5>
+
+      激活码1：
+      <p>
+      L9TOF0Zsj8SsYIOZmvKZLcPSWIAt4fX1+Zq9U+x/VoW
+      <br/>
+      reNxBYBekZAdhNylC/EVBLSLAKnls/w5zWtv4cd6E3d
+      <br/>
+      Tc1vca+teq6sKjjNA6SnF+OSps28cvDXcyyFL1KDHW/
+      <br/>
+      y6kpr75OQKtgUHcmRazEkO693240n4SxSm8BvoZVgE=
+      <br/>
+      </p>
+      <br/>
+
+      激活码2：
+      <p>
+      X/j5QoCJj4p7Vruh/6On98If3p5C5KFNceZxlhijAEo
+      <br/>
+      B/1a1d+n95+UVBXXvDlZnwvPycndwh71wbc4GqFD6Cu
+      <br/>
+      tvKJ+aChznD3OeX01jU4HXnYPIrX0aqtdPamYIA9CPX
+      <br/>
+      OOoJstJxUSF/vhMJJXX2MQTXYbpkRkwnSeXymZgOPI=
+      <br/>
+      </p>
+      <br/>
+
+      激活码3：
+      <p>
+      Vj12ZOxznUNkZ6yFa9/ewsfvDFgSVxpdaiTf2p6+gYj
+      <br/>
+      WkItoG6myM7BbquSs0Mb13Sxv/tPJGUjEJ0/8iU3tUK
+      <br/>
+      LWiu78N8w0mIlKCsxQhOUR0Y/T+F3btDRtUT3d3kNEi
+      <br/>
+      zUOLELx8o81G/jRorIOTqvDFsHQcQ2ZM1t0vW0VMOQ=
+      <br/>
+      </p>
+      <br/>
+
+      激活码4：
+      <p>
+      HiKU5i/tFzkBHv+1xJjGFvURiD0vmBMzMJxXJX3dvL1
+      <br/>
+      Ww567Cx2d8uC9OwzZ3JxNKBvxLmG9eMHKHCCZxYN7jK
+      <br/>
+      /LRLCj9Eehm3s9Z01uResc1X4c/E7fd5K2zIHuiqPbS
+      <br/>
+      uD5hYB7gFSBwpjuHE1ZjW09oEzT9z+sPI/F7lavHzA=
+      <br/>
+      </p>
+      <br/>
+
+      激活码5：
+      <p>
+      zdBVNAKC848USAFMRW00s+uUCeV80WxhDRVcR9+Ad8A
+      <br/>
+      QJcLL7G0UBq854On6e1DbkVVDMqYeR4bBwAamUstiNE
+      <br/>
+      gmwXHlVOqjDufUwxCP8kVI5hknJaPEFprnE4mvpVAyx
+      <br/>
+      WjYJZAu7zhvKkfb2IpXsDDPIPRuUeZS7LRcfJEnFLg=
+      <br/>
+      </p>
+      <br/>
+
+      激活码6：
+      <p>
+      C1cIlDX9YqDMRawDRXlI/G4pJ81xEXa7CCWQCR4miEI
+      <br/>
+      cunN2VC1l7rPnyK/FvHg7BiUKZS6V0/IOVfin8ZJn5M1
+      <br/>
+      wPiNObvG7hOWfvk9sP+rsi6Ruo/CgSpuqRdstiXf1c7
+      <br/>
+      CBkvEkqS4MVih8FmYmSJUB1uPuPoO387V94HLmjio=
+      <br/>
+      </p>
+      <br/>
+
       激活码7：
       <p>
-      HDpf3IBIbNcYJHuZGsceCpRlnKq3HzHFFZm69b/y8Cg08w9Gjjty6NpjyXkrEXaZJL5UqG6ZRcOZ
-      6pUOP8LauzmVA9qLhTbIliqqhEAuUbJlqER+fR0gA29OHvIDAFeXY+kW+yP9zD6U99j0/F9HJnnm
-      BW6EniVJFs0YcWZgaNY=
+      HDpf3IBIbNcYJHuZGsceCpRlnKq3HzHFFZm69b/y8Cg
+      <br/>
+      08w9Gjjty6NpjyXkrEXaZJL5UqG6ZRcOZ6pUOP8Lauz
+      <br/>
+      mVA9qLhTbIliqqhEAuUbJlqER+fR0gA29OHvIDAFeXY
+      <br/>
+      +kW+yP9zD6U99j0/F9HJnnmBW6EniVJFs0YcWZgaNY=
+      <br/>
       </p>
+      <br/>
 
       激活码8：
       <p>
-      uZ72yswqTo96P5+RzffouRP/FWq351xzmsfEOxh53C/uq01gdjw3tYstLbi0p0sy8g/7BT+pkmdP
-      Rr/v2oezFX4GIyEOQaU4HkeMI/mWn2wcdm/k8b/sGVnhh0FqHYYf61WOBWgTM286tLTWQ+Naxgp+
-      1z17ARvRDKuaqfhPv5c=
+      uZ72yswqTo96P5+RzffouRP/FWq351xzmsfEOxh53C/
+      <br/>
+      uq01gdjw3tYstLbi0p0sy8g/7BT+pkmdPRr/v2oezFX
+      <br/>
+      4GIyEOQaU4HkeMI/mWn2wcdm/k8b/sGVnhh0FqHYYf6
+      <br/>
+      1WOBWgTM286tLTWQ+Naxgp+1z17ARvRDKuaqfhPv5c=
+      <br/>
       </p>
+      <br/>
 
       激活码9：
       <p>
-      eABV+U605XdZZ8QjdIS9xVLDYIwk44KgZTm2EVmqCCgOws6tEA9zSjJWohiYshl+EcyVJMfKsAU9
-      eG1KQ7cFBt8NCw2WuJG2L9Z9hLurl5lYONFgeOzvEFVbNSBKD2R96MgRL4P1rOCzVQiexfb1cYTn
-      MeMqv9exdqnsdrFLxaw=
+      eABV+U605XdZZ8QjdIS9xVLDYIwk44KgZTm2EVmqCCg
+      <br/>
+      Ows6tEA9zSjJWohiYshl+EcyVJMfKsAU9eG1KQ7cFBt
+      <br/>
+      8NCw2WuJG2L9Z9hLurl5lYONFgeOzvEFVbNSBKD2R96
+      <br/>
+      MgRL4P1rOCzVQiexfb1cYTnMeMqv9exdqnsdrFLxaw=
+      <br/>
       </p>
+      <br/>
 
       激活码10：
       <p>
-      IdsNUGrPna2l3nk65cPclkCjlwPJ20AGa9ncCfFlaNIEhWu3XnrecjtLbVWqp+UsbVdUzTlru5Lq7
-      YWtgO9uVyp/Dy43gJZI7OqGXeFUKOUiwtN3IoWXmmo2diXv6tDpnymRfoOjbPcUUh3qPoCr7QGOm2
-      7Yf9Fep0m/RTgAXPY=
+      IdsNUGrPna2l3nk65cPclkCjlwPJ20AGa9ncCfFlaNI
+      <br/>
+      EhWu3XnrecjtLbVWqp+UsbVdUzTlru5Lq7YWtgO9uVy
+      <br/>
+      p/Dy43gJZI7OqGXeFUKOUiwtN3IoWXmmo2diXv6tDpn
+      <br/>
+      ymRfoOjbPcUUh3qPoCr7QGOm27Yf9Fep0m/RTgAXPY=
+      <br/>
       </p>
+      <br/>
+
+      激活码11：
+      <p>
+      vCA1FEiX9U4q+KKS1ZE7odrje0S5va9+tZsoA0Ec2Ze
+      <br/>
+      8Y+SOKRuo6Hue9Q+1hpxlQ+ipqaJPn4gwRuNcooBjVk
+      <br/>
+      ufmuJa5vaC1EGXJ5aDzrQ6XZ8KYmNCGPYoUHzMteaAx
+      <br/>
+      X6RuZPA3SBwBSLlKy9XRIYQFKAT1kXoUC7u2AAStb0=
+      <br/>
+      </p>
+      <br/>
+
+      激活码12：
+      <p>
+      WzHkEQIPAVGo1RreBSR9P9vtuPj6mmSvf9LzlgqSbUM
+      <br/>
+      MiIR7BlI0VQ0YXCMzTYFzS9NZzgbsVWOljABSHAjJug
+      <br/>
+      1eEjolxQYBAIyUkRC60I/rz39urQDainBMQnpT0KWJZ
+      <br/>
+      +lteEJ0Mm3uZ7yaGSdFtE2Kn4TXESLs+ZFifiRxhGs=
+      <br/>
+      </p>
+      <br/>
+
+      激活码13：
+      <p>
+      cTQw5E5Hzah+MDAe+1hqFShWaGSdqy247p/Cq/H9dhM
+      <br/>
+      Beuuaa36E+2Sg3d9N0JFxbiYEU/cWVfxNUH+zrHgxop
+      <br/>
+      Eqb1xjBlpZ/UCFBB0kCCxGWrSEaVggyRaZAZP4+UoD9
+      <br/>
+      VlSdV4GyjatRZbzc9TJMwCSjSUMpcseApVeYJTGgyU=
+      <br/>
+      </p>
+      <br/>
+
+      激活码14：
+      <p>
+      cusIRoNzUbKSqZzpGXQTz7HXh62+JKOPXuTuuKEjvi4
+      <br/>
+      76UJ97YxgNftDQdy2V1TT33RftOczHAq1LMqktrJb4j
+      <br/>
+      azQZFOrXxe6ZYZMYJc6h49WpwGtgyyQ+9G8LAMLWwPr
+      <br/>
+      IZvpTzjMN0qtUvBIq/c69iPCEjLYFTQJ3o1kQtOmoo=
+      <br/>
+      </p>
+      <br/>
+
+      激活码15：
+      <p>
+      hhEqltZpstmATIlwEGQ0gbM5f0y3CvRJlIbwBsto904
+      <br/>
+      Eq2qTZoCXhyMxy7ieatmawd1EmTx/7HBAGN1c9qDU07
+      <br/>
+      mn7kCrsugFD9nLzkSyumFSwhgoteBttPLAILG9cag8g
+      <br/>
+      +zTtU9GOuhPDm6zZ1tp2Wff8FjE0N7cB5ZXqNGF1I0=
+      <br/>
+      </p>
+      <br/>
+
+      激活码16：
+      <p>
+      fkf1CGMDM3QOzio3GwbsBPTsAroov3OiS7cMnKQ29f8
+      <br/>
+      hyIW0vnaR0ezXxI0xaOSrLWTTiCqUmQX3Y6ObMy66sy
+      <br/>
+      cvNuCkP9wat6r9ciF/9LMdUnHpmRoa+Vzv5/dSnL64v
+      <br/>
+      jPT0pKdJ0TYt0xWXOBl59mVJ+Zb11vuY9lbzB21NX0=
+      <br/>
+      </p>
+      <br/>
+
+      激活码17：
+      <p>
+      HHrwra5F13R4CBGIsGyIUr6UH/p+fEqq9w17zWM8aoP
+      <br/>
+      OSqx6g7TbGxIrYLnoRHRwwxw4vEnkrULbPNMY6ycv3r
+      <br/>
+      4oVhKgqX1vI8qqNPJfXWV5UsSNIhfVbzoifCX6FO3ia
+      <br/>
+      ariMx9tgoHvam3QhfePa7idhmVDuDiaZwBOWuZvntg=
+      <br/>
+      </p>
+      <br/>
+
+      激活码18：
+      <p>
+      w+pN59U6em0po18i7/43oh1Rv2Z/j08rz17uBzDpNAJ
+      <br/>
+      vKqNIkOy6beczbcVinM1d64sxZt1HX4c7MwXaa7WBWY
+      <br/>
+      MhTQZHc2po0S2pEh4w1FxggOqTvJDO/8f5vdDlLdsOD
+      <br/>
+      L64qUhIBgKsNXzRN2EUN6oB3RjkW613eCpyVIMhGcU=
+      <br/>
+      </p>
+      <br/>
+
+      激活码19：
+      <p>
+      3nYjQZx7qPU1GvJk9JdEMQ8LebeOZLoL1cvuyNJdzKY
+      <br/>
+      jaVYyc6IL+SX6ZAmhB5gvShna5CUO7BxhZQRXZ/1pe+
+      <br/>
+      5rJBKEwIvtlwInVmlDqBrbVRyqmLjDovvjRmW/+j/ni
+      <br/>
+      haSok1hz1rfg6+pnrBzzRCbr7JMT8wSov6pYY4iNzM=
+      <br/>
+      </p>
+      <br/>
+
+      激活码20：
+      <p>
+      EtnrzfkNnFoDMI/wrefX7vteS1QDExBiZHQwxoLPyFT
+      <br/>
+      ngYOwXd5QxWgnvRn51Oe8gcRkx0u0n82wA7+3PoHbnv
+      <br/>
+      tfJTddPnG57dKJsOjLai8UspiE8Oxo7wTssp458c3q5
+      <br/>
+      0KtbJYS7WF6WaHOgbiL/b389qRNJcKUCX0EwU4hTfc=
+      <br/>
+      </p>
+      <br/>
+
+      激活码21：
+      <p>
+      bZvhPZGfslkhazvymErn7bfqYCpJ/n+04nrut1PHMzv
+      <br/>
+      dwzq085c8m24YefvM/xoS0squiOS2Aad5GamoZsiqXN
+      <br/>
+      jRZT0ogs9uj7GidSTVtfLA1N5cMB2znPA1tEnbqgA26
+      <br/>
+      /mSWT0hxyIOhBws8p+HlPLwoAhoy/s+DKsCjYtsHm0=
+      <br/>
+      </p>
+      <br/>
+
+      激活码22：
+      <p>
+      1coy3McTAzHw48py390xltk3MNqOcfKQWAkgW7C6SIW
+      <br/>
+      SSBahpjmls3lIqS/2t9ZBLLtmk8v5zsy20QQwrl9xHN
+      <br/>
+      otps0khJnZ18XFCq7qov0sNSQ40VsTwHkVL6NlfBxkt
+      <br/>
+      uyvbHDzDTOnA7ERepZIbYqmHj6GwhwWBnmHdnium8s=
+      <br/>
+      </p>
+      <br/>
+
+      激活码23：
+      <p>
+      D4u3/mZ5neC7KS5GM90paWjFSMwTQhkeDwr7+AnjPx+
+      <br/>
+      a5uv1njMQoYO7x6K643IzIW42uftdm8w4sUuqolNpzV
+      <br/>
+      e9a0QnXEnJZu5jVBJXTPJQeXm563t8VIo1YDeTK9g/q
+      <br/>
+      2ve8OaY0NsDtF1Y052fHUYsvDFi2Bz5AOqR+xWy1+o=
+      <br/>
+      </p>
+      <br/>
+
+      激活码24：
+      <p>
+      zNnTgOYVPhKcaNWG1BUeFFsuD3LQb+Ovsfjce4Nd10Z
+      <br/>
+      qk9iq5bNdL4+JeDJtxwwROrj3CzBauk1XCvfHbKG9QM
+      <br/>
+      17eIBoSsdtEmU94Xo40eHJanfjazMfuws4mX59xCps4
+      <br/>
+      fYmPPzRjotAVA8yj83cg61Ip9iLL8nCXa3VJZckl3Q=
+      <br/>
+      </p>
+      <br/>
+
     </h5>
   </div>
 </template>
