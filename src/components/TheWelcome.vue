3a9644f25b529881a6418cdadc66b7289e1d0462
dora <dora924666990@gmail.com>
2024-04-06 16:36:54 +0800

    新增v1.5的包

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -13,7 +13,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Product Introduce</template>
+    <template #heading>Dora Box</template>
     <h6>
 Privacy is a basic right that everyone should be respected and protected. However, with the popularity of social media and digital communications,
 
@@ -34,30 +34,31 @@ Privacy is a basic right that everyone should be respected and protected. Howeve
      If you attach great importance to protecting personal privacy, then [Privacy Safe] is an application you must not miss. You can
 
      It splits private content and saves it in scattered ways to protect your privacy.
+     <p>Package Name : com.dorachat.privacybox</p>
      <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.0.apk">Dora Box v1.0(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.0.apk">Dora Box v1.0</a>
         MD5:7575550576575c3c35da5d082b0a00d7
      </p>
       <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.1.apk">Dora Box v1.1(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.1.apk">Dora Box v1.1</a>
         MD5:8bcdb082e43da631c79b12a6a202ae27
      </p>
      <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.2.apk">Dora Box v1.2(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.2.apk">Dora Box v1.2</a>
         MD5:92ad109b4348407de19b0a10e5e8b4f2
      </p>
      <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.3.apk">Dora Box v1.3(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.3.apk">Dora Box v1.3</a>
         MD5:97c996430b93dc58b501529e1ce64203
      </p>
 
      <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.4.apk">Dora Box v1.4(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.4.apk">Dora Box v1.4</a>
         MD5:b3ab8eabc7e4d18da384e3cc2b4097da
      </p>
 
      <p>
-        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.5.apk">Dora Box v1.5(com.dorachat.privacybox)</a>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.5.apk">Dora Box v1.5</a>
         MD5:0456d6a216ce2c005b97637efc443354
      </p>
 
@@ -67,12 +68,35 @@ Privacy is a basic right that everyone should be respected and protected. Howeve
 
   <WelcomeItem>
     <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Dora Chat</template>
+
+    <h6>
+Dora Chat is a social app that supports friend chat and group chat. If you have friends, you are never alone. Welcome to join the Dora Chat community.
+    <a href="https://www.pgyer.com/dorachat/" target="_blank">Details</a>
+    </h6>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
 
       <SupportIcon />
     </template>
     <template #heading>Contact Us</template>
+
+     <p>
+    <a href="https://twitter.com/bellabella10169">Twitter</a>
+     </p>
+     <p>
+    <a href="https://www.facebook.com/profile.php?id=61551819646749">Facebook</a>
+     </p>
+     <p>
+    <a href="https://www.youtube.com/channel/UCtlq1G4CwMu7RXXNq-w9ZhA">Youtube</a>
+     </p>
      <p>
     <a href="https://github.com/dora4">Github</a>
      </p>
+
   </WelcomeItem>
 </template>
