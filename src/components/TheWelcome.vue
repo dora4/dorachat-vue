62425323b27f1af7907a2fcff790b1e37229a3cf
dora <dora924666990@gmail.com>
2024-09-03 02:28:23 +0800

    新增tiktok和油管

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -11,18 +11,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Dora Box</template>
-
-    <p>
-        <a href="http://doracrypto.com">已离开Dora Chat，点此访问</a>
-    </p>
-
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
       <CommunityIcon />
     </template>
     <template #heading>Dora Chat</template>
@@ -31,16 +19,24 @@ Dora Chat is a social app that supports friend chat and group chat. If you have 
     <a href="https://www.pgyer.com/dorachat/" target="_blank">Details</a>
   </WelcomeItem>
 
+
+      <WelcomeItem>
+        <template #icon>
+          <ToolingIcon />
+        </template>
+        <template #heading>Encryption and decryption tools for private information</template>
+
+        <p>
+            <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/app-dora-release.apk">To my future self</a>
+        </p>
+
+      </WelcomeItem>
+
   <WelcomeItem>
     <template #icon>
-
       <SupportIcon />
     </template>
     <template #heading>Contact Us</template>
-
-     <p>
-    <a href="https://www.youtube.com/channel/UCEDRPkqMGeBN3UiqLC6Qj-g">Youtube</a>
-     </p>
      <p>
          <a href="https://discord.com/channels/1234248413701476352/1234248413701476354">Discord</a>
      </p>
@@ -48,11 +44,16 @@ Dora Chat is a social app that supports friend chat and group chat. If you have 
     <a href="https://github.com/dora4">Github</a>
      </p>
      <p>
-    <a href="https://juejin.cn/user/2656895088205453">稀土掘金技术社区</a>
+    <a href="https://juejin.cn/user/2656895088205453">Juejin</a>
      </p>
-
      <p>
     <a href="https://blog.csdn.net/a_lwh____?spm=1011.2480.3001.5343">CSDN</a>
+     </p>
+     <p>
+    <a href="https://www.tiktok.com/@dorachat.com">Tiktok</a>
+     </p>
+     <p>
+    <a href="https://www.youtube.com/@dorachat01">Youtube</a>
      </p>
   </WelcomeItem>
 </template>
